fd1c967ba11ae2be70914eb7c137e1a157037cf1
new00hurt <new00hurt@gmail.com>
2020-03-21 20:45:58 +0200

    firstCommit

---
+++ src/task2/Task2.java
@@ -0,0 +1,16 @@
+
+package task2;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        int x = 125;
+        int y = 24;
+        System.out.println(x + " + " + y + " = " + (x+y));
+        System.out.println(x + " - " + y + " = " + (x-y));
+        System.out.println(x + " × " + y + " = " + (x * y));
+        System.out.println(x + " / " + y + " = "  + (x / y));
+        System.out.println(x + " mod " +y + " = " + (x % y)); 
+    }
+    
+}
